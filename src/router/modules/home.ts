8c0b462f25405a0b214581339df250c63f6bfffe
minghua <minghua@gmail.com>
2022-11-07 17:52:46 +0630

    Modify

--- src/router/modules/home.ts
+++ src/router/modules/home.ts
@@ -9,7 +9,8 @@ export const homeRouters = [
       icon: "./images/tabbar/home.png",
       icon_select: "./images/tabbar/home_select.png",
     },
-    component: () => import("/@/views/newHome/index.vue"),
+    // component: () => import("/@/views/Home/index.vue"),
+    component: () => import("./../../views/Home/index.vue"),
   },
 ];
 
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="container">This is home page</div>
+  <div class="container">
+    <h1>This is home page</h1>
+  </div>
 </template>
 
-<script lang="ts" setup>
-import {
-  defineComponent,
-  toRefs,
-  reactive,
-  onMounted,
-  computed,
-  ref,
-} from "vue";
+<script lang="ts">
+import { defineComponent, toRefs, reactive, onMounted } from "vue";
 
-onMounted(() => {});
+export default defineComponent({
+  name: "home",
+  layout: "home",
+  components: {},
+  setup() {
+    const state = reactive({});
+
+    onMounted(() => {});
+    return {
+      ...toRefs(state),
+    };
+  },
+});
 </script>
 
 <style lang="scss" scoped></style>
