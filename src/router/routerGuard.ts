656b0b40b76ba30bd5d40e3971a7f542bc69a954
minghua <minghua@gmail.com>
2022-11-08 15:02:32 +0630

    Delete

--- src/router/routerGuard.ts
+++ src/router/routerGuard.ts
@@ -8,7 +8,6 @@ export function createRouterGuards(router: Router) {
   router.beforeEach((to, _from, next) => {
     const { path } = to;
     const token = store.state.user.token;
-    const userdata = store.state.user.user;
     const isWhite = list.includes(path);
     if (!token) {
       if (!isWhite) {
